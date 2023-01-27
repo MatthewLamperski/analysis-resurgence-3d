eaa4c280bbb10c096b912211d18532d7de4c7f82
MatthewLamperski <matthew.lamperski@gmail.com>
2023-01-27 15:18:37 -0500

    update build action

--- .github/scripts/add-osx-cert.sh
+++ .github/scripts/add-osx-cert.sh
@@ -15,7 +15,7 @@ echo 2
 security unlock-keychain -p actions $KEY_CHAIN
 echo 3
 
-security import $CERTIFICATE_P12 -k $KEY_CHAIN $MAC_CERTS_PASSWORD -T /usr/bin/codesign
+security import $CERTIFICATE_P12 -k $KEY_CHAIN -P $MAC_CERTS_PASSWORD -T /usr/bin/codesign
 echo 4
 security set-key-partition-list -S apple-tool:,apple: -s -k actions $KEY_CHAIN
 echo 5
